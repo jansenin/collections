a6432e1559b4b7b6f79c72a83258db94ce907a10
jansenin <59775038+jansenin@users.noreply.github.com>
2020-01-11 23:26:56 +0300

    Add files via upload

---
+++ solution/UniqueElementsUsingMap.js
@@ -0,0 +1,40 @@
+var arr = [
+    1,
+    2,
+    3,
+    4,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    1,
+    2,
+    3,
+    4,
+    2,
+    4,
+    5,
+    5,
+    7,
+    6,
+    5,
+    4,
+    3
+];
+function getUnique(arr) {
+    let map = new Map();
+
+    for (var i = 0; i < arr.length; i++) {
+        map.set(arr[i], 1);
+    }
+
+    let result = [];
+    for (let key of map.keys()) {
+        result.push(key);
+    }
+    return result;
+}
+console.log(arr);
+console.log(getUnique(arr));
