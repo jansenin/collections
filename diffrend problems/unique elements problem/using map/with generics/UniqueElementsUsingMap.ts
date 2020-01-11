aa082c89f1bbc702ec3d13beb9fc14ba4401ee5c
jansenin <59775038+jansenin@users.noreply.github.com>
2020-01-12 01:10:34 +0300

    Add files via upload

---
+++ diffrend problems/unique elements problem/using map/with generics/UniqueElementsUsingMap.ts
@@ -0,0 +1,54 @@
+type T = [number, string];
+
+let arr: T[] = [
+  [1, "1"],
+  [2, "2"],
+  [3, "3"],
+  [2, "2"],
+  [4, "1"],
+  [1, "1"],
+  [4, "4"],
+  [3, "1"]
+];
+
+/*let arr: T[] = [
+  1,
+  2,
+  3,
+  4,
+  2,
+  3,
+  1,
+  2,
+  3,
+  4,
+  1,
+  2,
+  3,
+  4,
+  2,
+  4,
+  5,
+  5,
+  7,
+  6,
+  5,
+  4,
+  3
+];*/
+
+function getUnique(arr: T[]) {
+  let i = 1;
+  let map = new Map<T, number>(arr.map(val => [val, i++]));
+
+  console.log(map);
+  console.log(map.get([1, "1"]));
+
+  let result: T[] = [];
+  for (let key of map.keys()) {
+    result.push(key);
+  }
+  return result;
+}
+console.log(arr);
+console.log(getUnique(arr));
